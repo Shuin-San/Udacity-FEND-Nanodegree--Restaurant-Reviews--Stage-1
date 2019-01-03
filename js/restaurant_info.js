1439eecda53b9927861f00038c0a50c38c9721cf
Shuin <shuingr@gmail.com>
2019-01-03 14:06:54 +0100

    added main page responsiveness

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic2h1aW5zYW4iLCJhIjoiY2pxZ2k2cDZzMjBncTQybGJ0N3RuNTZsMCJ9.sEt7jN38UT0Q9SsFeMzzkg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
